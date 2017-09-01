8727a8fc41a2bb860f32b50e4ca49b325d32223c
erochOnGit <emile31.roch@gmail.com>
2017-09-01 10:38:14 +0200

    .test add pixi frame and dependencies for graphql

--- nodeExemple.js
+++ nodeExemple.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('static'));
+// app.use(express.staticProvider(__dirname));
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.render('index.html');
+
 });
 
 app.listen(3000, function () {
---
+++ src/schema/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by emile on 01/09/2017.
+ */
